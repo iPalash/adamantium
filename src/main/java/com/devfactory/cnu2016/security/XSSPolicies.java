fb0b194d1b3c2551fb313d576fd875eba6f5e4da
Swapnil Palash <swapnil.palash@codenation.co.in>
2016-08-26 22:59:17 +0400

    Added travis support

--- src/main/java/com/devfactory/cnu2016/security/XSSPolicies.java
+++ src/main/java/com/devfactory/cnu2016/security/XSSPolicies.java
@@ -1,7 +1,10 @@
 package com.devfactory.cnu2016.security;
-
 /**
  * Created by swapnil on 26/08/16.
  */
+import org.owasp.esapi.ESAPI;
 public class XSSPolicies {
+    public static String cleanHTML(String input){
+        return ESAPI.encoder().encodeForHTML(input);
+    }
 }
--- src/test/java/com/devfactory/cnu2016/security/XSSPoliciesTest.java
+++ src/test/java/com/devfactory/cnu2016/security/XSSPoliciesTest.java
@@ -1,7 +1,20 @@
 package com.devfactory.cnu2016.security;
 
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
 /**
  * Created by swapnil on 26/08/16.
  */
 public class XSSPoliciesTest {
+    Logger LOG= LoggerFactory.getLogger(XSSPoliciesTest.class);
+    @Test
+    public void testCleanHTML(){
+        String badHTML="something with<script>do stuff</script>bad code";
+        String goodHTML=XSSPolicies.cleanHTML(badHTML);
+        LOG.info(badHTML+" : "+goodHTML);
+        assert(goodHTML.contains("<script>")==false);
+    }
 }
